e2109acc6fa612a3f8c530e093a37ffaa61fd89d
davidgq10 <davidgq10@hotmail.com>
2021-03-31 03:10:49 +0000

    entrega

--- src/models.py
+++ src/models.py
@@ -8,23 +8,50 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table user
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    mail = Column(String(100), nullable=False)
+    password = Column(String(15), nullable=False)
+
+class Character(Base):
+    __tablename__ = 'character'
+    # Here we define columns for the table character.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    gender = Column(String(15), nullable=True)
+    hair_color = Column(String(100), nullable=True)
+    eyes_color = Column(String(30), nullable=True)
+    birth_year = Column(String(10), nullable=False)
+    height = Column(Integer, nullable = False)
+    skin_color = Column(String(100),nullable=True)
+    description = Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+class Planet(Base):
+    __tablename__ = 'planet'
+    # Here we define columns for the table planet.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String (250),nullable=False)
+    population = Column(Integer,nullable=False)
+    terrain = Column(String(250), nullable=False)
+    climate = Column(String)
+    orbital_period = Column(Integer)
+    rotacion_period = Column(Integer)
+    diameter = Column (Integer, nullable = False)
+    description = Column(String)
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    # Here we define columns for the table favorite.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    type_favorite = Column(Integer,nullable = False)
+    relationship_id = Column(Integer,nullable=False)
 
     def to_dict(self):
         return {}
